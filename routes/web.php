047b0966a262029b790ca7ec470a5774ceaf16af
Mohamed Alaa <medo2012421@gmail.com>
2021-09-18 16:25:35 +0200

    make read all students by Yajra Datatables

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\StudentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('students', [StudentController::class, 'index']);
+
+Route::get('students/list', [StudentController::class, 'getStudents'])->name('students.list');
